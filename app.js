c40efe9e2b71e292d661fa3066795d23f90a68d8
Nahuel Quispe <riverfisica@gmail.com>
2024-01-16 23:55:11 -0300

    se agrega las rutas de login y register

--- app.js
+++ app.js
@@ -12,4 +12,17 @@ app.listen(3000, () => {
 app.get("/shopping-cart", (req, res) => {
     const path_shopping_cart = path.resolve(__dirname, "views/shopping-cart.html");
     res.sendFile(path_shopping_cart);
+});
+app.get("/login", (req, res) => {
+    const path_login = path.resolve(__dirname, "views/login.html");
+    res.sendFile(path_login);
+});
+app.get("/login", (req, res) => {
+    const path_login = path.resolve(__dirname, "views/login.html");
+    res.sendFile(path_login);
+});
+app.get("/register", (req, res) => {
+    const path_register = path.resolve(__dirname, "views/registro.html");
+    res.sendFile(path_register);
 })
+
