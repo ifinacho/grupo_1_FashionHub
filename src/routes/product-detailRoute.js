7b88fc1c9ad6f55690ea369cc188d83b94fe4d26
Ignacio Francisco Ibañez <endermindkai@gmail.com>
2024-02-20 20:39:37 -0300

    Realizando cambios en rutas y nombre de variable pasando de ProductId a product

--- src/routes/product-detailRoute.js
+++ src/routes/product-detailRoute.js
@@ -2,8 +2,8 @@ const express = require("express");
 const productsController = require("../controllers/productsController");
 const router = express.Router();
 
-router.get("/", productsController.details);
+router.get("/:id", productsController.details);
 router.get("/Create-product", productsController.create);
-router.get("/edit-product", productsController.edit);
+router.get("/:id/edit-product", productsController.edit);
 
 module.exports = router;
