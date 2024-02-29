b3eba66ee0b724fe1606c28b3c0c1a9b374bf2fd
Ignacio Francisco Ibañez <endermindkai@gmail.com>
2024-02-28 22:19:33 -0300

    reorganizando codigo en app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const path = require("path");
 const methodOverride = require('method-override');
 const app = express();
+const rutaShoppingCart = require("./routes/shopping-cartRoute.js");
+const rutaUser = require("./routes/userRoute.js");
+const rutaHome = require("./routes/homeRoute.js");
+const rutaProducts = require("./routes/product-detailRoute.js");
+const rutaCreate = require("./routes/CreateRoute.js")
 
 /*const ruta_public = path.resolve(__dirname, "./public");
 app.use(express.static(ruta_public));*/
@@ -14,26 +19,16 @@ app.use(methodOverride('_method'));
 app.set('view engine',  'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
+
+app.use("/shopping-cart", rutaShoppingCart);
+app.use("/user", rutaUser);
+app.use("/", rutaHome);
+app.use("/product-detail", rutaProducts );
+app.use("/create", rutaCreate)
+
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto: http://localhost:${PORT}`)
 })
-
-const rutaShoppingCart = require("./routes/shopping-cartRoute.js");
-app.use("/shopping-cart", rutaShoppingCart);
-
-const rutaUser = require("./routes/userRoute.js");
-app.use("/user", rutaUser);
-
-const rutaHome = require("./routes/homeRoute.js");
-app.use("/", rutaHome);
-
-const rutaProducts = require("./routes/product-detailRoute.js");
-app.use("/product-detail", rutaProducts );
-
-const rutaCreate = require("./routes/CreateRoute.js")
-app.use("/create", rutaCreate)
-// Create-product
-
-
