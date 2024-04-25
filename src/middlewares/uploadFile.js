fec27d2795588c1d8612845f73eee0699f1aa147
Nahuel Quispe <riverfisica@gmail.com>
2024-04-25 11:12:18 -0300

    uploadFile ahora permite rutas dinamicas

--- src/middlewares/uploadFile.js
+++ src/middlewares/uploadFile.js
@@ -1,9 +1,19 @@
 const multer = require("multer");
 const path = require("path");
 
+const getUploadDestination = (req) =>{
+   if (req.originalUrl.includes("/product")){
+      return "productos";
+   } else if (req.originalUrl.includes("/user")){
+      return "usuarios"
+   }
+}
 const storage = multer.diskStorage({ 
     destination: function (req, file, cb) { 
-       cb(null, path.join(__dirname, '../../public/img/productos')); 
+      const basePath = path.join(__dirname, '../../public/img/'); 
+      const destination = getUploadDestination(req);
+      const destPath = path.join(basePath, destination);
+       cb(null, destPath); 
     }, 
     filename: function (req, file, cb) { 
        cb(null, `IMG_${Date.now()}_${file.originalname}`);  } 
