0ade054562a62a60ef17cba4da02560a3a426663
Nahuel Quispe <riverfisica@gmail.com>
2024-06-11 21:37:59 -0300

    correciones menores

--- src/controllers/createProductController.js
+++ src/controllers/createProductController.js
@@ -1,13 +1,10 @@
-const fs = require('fs');
-const path = require('path');
 const crypto = require('crypto')
 const db = require("../database/models/")
-const productsFilePath = path.join(__dirname, '../data/productsDataBase.json')
-let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'))
+
 
 const controller= {
     create: (req, res)=> {
-        res.render('create-product')
+        res.render('create-product');
     },
     createPost: (req,res)=>{
         db.Product.create({
@@ -26,26 +23,6 @@ const controller= {
         }).catch(error => {
             console.error(error);
         });
-
-        /*let newProduct = {
-			id: crypto.randomUUID(),//newid
-			name: req.body.name,
-			price: Number(req.body.price),
-			discount: Number(req.body.discount),
-			color: req.body.color,
-            talle: req.body.talle,
-			description: req.body.description,
-            image: req.file.filename,
-            category: req.body.category			
-		}
-
-        console.log(newProduct)
-
-        products.push(newProduct)
-
-        fs.writeFileSync(productsFilePath, JSON.stringify(products,null, 4))
-        
-        */
     },
 }
 
