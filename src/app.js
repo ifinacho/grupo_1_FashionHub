9ac503ad26e36ff109747086ef6de0f7ce8a29dc
Nahuel Quispe <riverfisica@gmail.com>
2024-06-15 03:38:21 -0300

    cambios minimos en app.js, home mas dinamico, agregar producto no permitido si no esta logueado, section-interest dinamico

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ const path = require("path");
 const methodOverride = require('method-override');
 const userMiddleware = require("./middlewares/userMiddleware.js")
 const app = express();
-const rutaShoppingCart = require("./routes/shopping-cartRoute.js");
 const rutaUser = require("./routes/userRoute.js");
 const rutaHome = require("./routes/homeRoute.js");
 const rutaProducts = require("./routes/product-detailRoute.js");
@@ -14,7 +13,7 @@ const rutaCreate = require("./routes/CreateRoute.js");
 const db = require("../src/database/models");
 
 app.use(express.static("public"));
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(methodOverride('_method'));
 
@@ -26,14 +25,13 @@ app.use(session({
 app.use(cookies());
 app.use(userMiddleware);
 
-app.set('view engine',  'ejs');
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 
 app.use("/create", rutaCreate)
 app.use("/", rutaHome);
 app.use("/product-detail", rutaProducts);
-app.use("/shopping-cart", rutaShoppingCart);
 app.use("/user", rutaUser);
 app.use((req, res, next) => {
     res.status(404).render("404error");
@@ -42,6 +40,14 @@ app.use((req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 db.sequelize.sync();
+
+/*function generateSQLScript(){
+    const queries = db.sequelize.getQueryInterface().showCreateQueriesForTables();
+    for (const table in queries){
+        console.log(queries[table]);
+    }
+}
+generateSQLScript();*/
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto: http://localhost:${PORT}`);
 });
--- src/routes/shopping-cartRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const shoppingCartController = require("../controllers/shoppingCartController");
-const router = express.Router();
-
-router.get("/", shoppingCartController.index);
-router.post("/", shoppingCartController.checkout);
-module.exports = router;
